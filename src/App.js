5cfea25e209604055d672891f0d5f7aa2e824e1f
wendyyanto <yanto.wendy98@gmail.com>
2020-12-06 20:15:39 +0700

    verify issue on code markdown

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import DOMPurify from 'dompurify';
 import marked from 'marked';
 
 function App() {
-  const markedDown = DOMPurify.sanitize('# Marked in Node.js \n\n Rendered by **marked**.')
+  const markedDown = DOMPurify.sanitize('# Heading 1 \n ## Heading 2 \n [I\'m an inline-style link](https://www.google.com) \n\n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1") ')
   const html = marked(markedDown);
+
+  const jsCode = '```javascript \n var s = "JavaScript syntax highlighting"; \n alert(s);```'
+
+  const codeMarkedDown = DOMPurify.sanitize(jsCode)
+  const codeHtml = marked(codeMarkedDown);
 
   return (
     <div className="App">
@@ -15,6 +20,10 @@ function App() {
           Example of Marked down usage
         </h2>
         <div dangerouslySetInnerHTML={{__html: html}}></div>
+        <h2>
+          Example of Code
+        </h2>
+        <div dangerouslySetInnerHTML={{__html: codeHtml}}></div>
       </header>
     </div>
   );
