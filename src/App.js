1d08cb2651e8c9ca349a17fbc92dc67b7b7d3775
wendyyanto <yanto.wendy98@gmail.com>
2020-12-06 20:00:55 +0700

    setup demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import DOMPurify from 'dompurify';
+
+const marked = require("marked");
+
+
 function App() {
+  const markedDown = DOMPurify.sanitize('# Marked in Node.js \n\n Rendered by **marked**.')
+  const html = marked(markedDown);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Example of Marked down usage
+        </h2>
+        <div dangerouslySetInnerHTML={{__html: html}}></div>
       </header>
     </div>
   );
